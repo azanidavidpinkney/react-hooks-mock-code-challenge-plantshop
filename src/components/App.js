199aaa8468ab6d0318305f225599aa945cf6533e
Azani David Pinkney <info@azani.org>
2023-11-29 12:42:17 -0800

    plants lab

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,16 @@ import React from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
+// App
+// |- Header
+// |- Plant Page (retrieves plants data from API)
+//    |- NewPlantForm
+//    |- Search
+//    |- PlantList (receives plants data from Page)
+//       |- PlantCard (receives plant data from List)
+
 function App() {
+
   return (
     <div className="app">
       <Header />
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState , useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
+const plantsAPI = "http://localhost:6001/plants"
+
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    fetch(plantsAPI)
+    .then(response => response.json())
+    .then((data) => setPlants(data))
+  }, [])
+
+  function addNewPlant(newPlant) {
+    setPlants([...plants, newPlant])
+  }
+
+  function handleInventory(plant) {
+      const updatedPlants = plants.map(p => p.id !== plant.id ? p : {...p, outOfStock: true})
+      setPlants(updatedPlants)
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm plantsAPI={plantsAPI} addNewPlant={addNewPlant}/>
+      <Search setFilter={setFilter} />
+      <PlantList plants={plants.filter(plant => plant.name.startsWith(filter))} handleInventory={handleInventory} />
     </main>
   );
 }
